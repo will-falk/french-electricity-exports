d7d8884d3321fb88b7a4de300b024b0b1146c831
will falk <will@heueso.com>
2026-02-27 15:04:05 -0500

    add norway

--- first_call.py
+++ first_call.py
@@ -5,9 +5,15 @@ API_KEY = "***REMOVED***"
 
 client = EntsoePandasClient(api_key=API_KEY)
 
-start = pd.Timestamp('20171201', tz='Europe/Brussels')
-end = pd.Timestamp('20180101', tz='Europe/Brussels')
-country_code = 'FR'
+start = pd.Timestamp('20250101', tz='Europe/Brussels')
+end = pd.Timestamp.now(tz='Europe/Brussels') - pd.Timedelta(days=1)
+# end = pd.Timestamp('20251231', tz='Europe/Brussels')
 
-all_borders = client.query_physical_crossborder_allborders(country_code, start, end, export=True, per_hour=True)
-all_borders = client.query_physical_crossborder_allborders(country_code, start, end, export=False, per_hour=True)
+fr_country_code = 'FR'
+no_country_code = 'NO'
+
+fr_all_borders_outbound = client.query_physical_crossborder_allborders(country_code, start, end, export=True, per_hour=True)
+fr_all_borders_inbound = client.query_physical_crossborder_allborders(country_code, start, end, export=False, per_hour=True)
+
+no_all_borders_outbound = client.query_physical_crossborder_allborders(country_code, start, end, export=True, per_hour=True)
+no_all_borders_inbound = client.query_physical_crossborder_allborders(country_code, start, end, export=False, per_hour=True)
