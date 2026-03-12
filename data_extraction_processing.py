f429abd1afc61cd97bd97c175673efad2f95c9c4
will falk <will@heueso.com>
2026-03-12 17:11:46 -0400

    fix net calcs, add test exports, initial resampling for time periods

--- data_extraction_processing.py
+++ data_extraction_processing.py
@@ -44,15 +44,47 @@ for zone in no_bidding_zones:
 
 
 if check_inbound_outbound_frames(fr_bidding_zone, fr_outbound, fr_inbound):
-    fr_net = fr_outbound + fr_inbound
+    fr_net = fr_inbound - fr_outbound
 
 no_net = {}
 for key in no_bidding_zones:
     if check_inbound_outbound_frames(key, no_outbound[key], no_inbound[key]):
-        no_net[key] = no_outbound[key] + no_inbound[key]
-        
-        
-   
-  
+        no_net[key] = no_inbound[key] - no_outbound[key]
+
+time_periods = {
+    "daily": "D",
+    "weekly": "W",
+    "monthly": "ME"
+}
+
+# build fr dictionary for export
+fr_net_frames_by_period = {}
+for key, rule in time_periods.items():
+    fr_net_frames_by_period[key] = fr_net.resample(rule).sum()
+
+fr_net.to_csv("fr_net.csv", index=True)
+
+for period, df in fr_net_frames_by_period.items():
+    df.to_csv(f"fr_net_{period}.csv", index=True)
+
+# build no dictionary for export
+no_net_frames_by_period = {}
+for key, rule in time_periods.items():
+    fr_net_frames_by_period[key] = fr_net.resample(rule).sum()
+
+fr_net.to_csv("fr_net.csv", index=True)
+
+for period, df in fr_net_frames_by_period.items():
+    df.to_csv(f"fr_net_{period}.csv", index=True)
     
+for zone, df in no_net.items():
+    df.to_csv(f"no_net_{zone}.csv", index=True)
     
+fr_outbound.to_csv(f"fr_out.csv", index=True)
+fr_inbound.to_csv(f"fr_in.csv", index=True)
+    
+no_outbound["NO_1"].to_csv(f"no_out_NO_1.csv", index=True)
+no_inbound["NO_1"].to_csv(f"no_in_NO_1.csv", index=True)
+
+no_outbound["NO_2"].to_csv(f"no_out_NO_2.csv", index=True)
+no_inbound["NO_2"].to_csv(f"no_in_NO_2.csv", index=True)
